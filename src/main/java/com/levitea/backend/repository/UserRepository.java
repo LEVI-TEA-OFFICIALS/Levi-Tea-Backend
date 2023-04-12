5c7fe46409eb62355599dfb701ce62674eb9c061
kanishak garg <kanishakgarg8778@gmail.com>
2023-04-12 14:57:12 +0530

    Create User Repository

--- src/main/java/com/levitea/backend/repository/UserRepository.java
+++ src/main/java/com/levitea/backend/repository/UserRepository.java
@@ -1,2 +1,9 @@
-package com.levitea.backend.repository;public interface UserRepository {
+package com.levitea.backend.repository;
+
+import com.levitea.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//Jpa repository needs 2 arguments one is model and datatype of primary key of model
+public interface UserRepository extends JpaRepository<User,Long> {
+
 }
