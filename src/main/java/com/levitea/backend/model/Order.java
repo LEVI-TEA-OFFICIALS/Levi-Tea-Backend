f12b18416b5ef2a77583671f2a63ffb3fd385f1f
kanishak garg <kanishakgarg8778@gmail.com>
2023-05-01 14:14:09 +0530

    create order schema

--- src/main/java/com/levitea/backend/model/Order.java
+++ src/main/java/com/levitea/backend/model/Order.java
@@ -1,2 +1,47 @@
-package com.levitea.backend.model;public class Order {
+package com.levitea.backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import org.springframework.data.annotation.Id;
+
+@Entity
+public class Order {
+    @Id
+    @GeneratedValue
+    private Long Id;
+    private Product product;
+    private double quantity;
+    private String price;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(double quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
 }
