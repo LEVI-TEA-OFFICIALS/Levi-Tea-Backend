c712076fd51006ddd1523b52073aa5376c0f830c
kanishak garg <kanishakgarg8778@gmail.com>
2023-08-07 21:19:06 +0530

    minor fix

--- src/main/java/com/levitea/backend/controller/UserController.java
+++ src/main/java/com/levitea/backend/controller/UserController.java
@@ -24,55 +24,55 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @PostMapping("/user")
-    User newUser(@RequestBody User newUser){
-        return userRepository.save(newUser);
-    }
-
-    @GetMapping("/users")
-    List<User> getAlllUsers(){
-        return userRepository.findAll();
-    }
+//    @PostMapping("/user")
+//    User newUser(@RequestBody User newUser){
+//        return userRepository.save(newUser);
+//    }
+//
+//    @GetMapping("/users")
+//    List<User> getAlllUsers(){
+//        return userRepository.findAll();
+//    }
 
     @GetMapping("/getUserById/{id}")
     Optional<User> getUserById(@PathVariable("id") Long id){return userService.getUserById(id);}
+
+    @GetMapping("/getUserByUsername/{username}")
+    Optional<User> getUserByUsername(@PathVariable("username") String username){return Optional.ofNullable(userService.getUserByUsername(username));}
+
+    @DeleteMapping ("/deleteUserById/{id}")
+    Optional<User> deleteUserById(@PathVariable("id") Long id){return userService.deleteUserById(id);}
+
+
+//      @GetMapping("/user/{id}")
+//      User getUserById(@PathVariable Long id){
+//        return userRepository.findById(id)
+//                .orElseThrow(()-> new UserNotFoundException(id));
+//      }
 //
-////    @GetMapping("/getUserByUsername/{username}")
-////    Optional<User> getUserByUsername(@PathVariable("username") String username){return Optional.ofNullable(userService.getUserByUsername(username));}
+//      @PutMapping("/user/{id}")
+//      User updateUser(@RequestBody User newUser,@PathVariable long id){
+//        return userRepository.findById(id)
+//                .map(user->{
+//                    user.setName(newUser.getName());
+//                    user.setUsername(newUser.getUsername());
+//                    user.setRank(newUser.getRank());
+//                    user.setAddress(newUser.getAddress());
+//                    user.setCity(newUser.getCity());
+//                    user.setPhoneNo(newUser.getPhoneNo());
+//                    return userRepository.save(user);
+//                }).orElseThrow(()->new UserNotFoundException(id));
+//      }
 //
-//    @DeleteMapping ("/deleteUserById/{id}")
-//    Optional<User> deleteUserById(@PathVariable("id") Long id){return userService.deleteUserById(id);}
-
-
-      @GetMapping("/user/{id}")
-      User getUserById(@PathVariable Long id){
-        return userRepository.findById(id)
-                .orElseThrow(()-> new UserNotFoundException(id));
-      }
-
-      @PutMapping("/user/{id}")
-      User updateUser(@RequestBody User newUser,@PathVariable long id){
-        return userRepository.findById(id)
-                .map(user->{
-                    user.setName(newUser.getName());
-                    user.setUsername(newUser.getUsername());
-                    user.setRank(newUser.getRank());
-                    user.setAddress(newUser.getAddress());
-                    user.setCity(newUser.getCity());
-                    user.setPhoneNo(newUser.getPhoneNo());
-                    return userRepository.save(user);
-                }).orElseThrow(()->new UserNotFoundException(id));
-      }
-
-      @DeleteMapping("user/{id}")
-      String deleteUser(@PathVariable Long id){
-        if(!userRepository.existsById(id)){
-            throw new UserNotFoundException(id);
-        }
-
-        userRepository.deleteById(id);
-        return "User with id "+id+" has been deleted succesfully  ";
-      }
+//      @DeleteMapping("user/{id}")
+//      String deleteUser(@PathVariable Long id){
+//        if(!userRepository.existsById(id)){
+//            throw new UserNotFoundException(id);
+//        }
+//
+//        userRepository.deleteById(id);
+//        return "User with id "+id+" has been deleted succesfully  ";
+//      }
 
 
 
