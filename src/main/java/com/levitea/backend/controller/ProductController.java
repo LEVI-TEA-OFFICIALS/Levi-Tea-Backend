6bb8ebcb5c873a441ec7662b261cc4707a0cb643
kanishak garg <kanishakgarg8778@gmail.com>
2023-11-27 23:39:42 +0530

    create product controller

--- src/main/java/com/levitea/backend/controller/ProductController.java
+++ src/main/java/com/levitea/backend/controller/ProductController.java
@@ -1,7 +1,36 @@
 package com.levitea.backend.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.levitea.backend.dao.model.Product;
+import com.levitea.backend.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class ProductController {
+
+    @Autowired
+    ProductService productService;
+
+    @PostMapping
+    public Product addProduct(@RequestBody Product product){
+        return productService.addProduct(product);
+    }
+
+    @DeleteMapping
+    public void deleteProductById(@RequestBody Long id){
+        productService.deleteProductById(id);
+    }
+
+    @PutMapping
+    public Product updateProduct(@RequestBody Product product){
+        return productService.updateProduct(product);
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts(){
+        return productService.getAllProducts();
+    }
+
 }
